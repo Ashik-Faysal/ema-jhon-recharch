8b23061bcdfb54482153d83961080b8c0fde9051
Ashik-Faysal <ashikfaysal.rc@gmail.com>
2023-03-28 22:26:13 +0600

    added fontAwesome icon

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import "./Product.css";
+  import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+  import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 
 const Product = (props) => {
-  const { img, name, seller, ratings, price } = props.product;
+    const { img, name, seller, ratings, price } = props.product;
+    
+    const handleAddToCart = props.handleAddToCart;
+    
   return (
     <div className="product">
       <img src={img ? img : "../../assets/image-4.jpg"} alt="" />
@@ -11,8 +16,14 @@ const Product = (props) => {
         <p>Price: ${price}</p>
         <p>Manufacturer: {seller}</p>
         <p>Rating : {ratings} stars</p>
-          </div>
-          <button className="btn-cart">Add to cart</button>
+      </div>
+      <button
+        onClick={() => handleAddToCart(props.product)}
+        className="btn-cart"
+      >
+        Add to cart
+        <FontAwesomeIcon icon={faShoppingCart} />
+      </button>
     </div>
   );
 };
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -4,21 +4,30 @@ import "./Shop.css";
 
 const Shop = () => {
   const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState([]);
   useEffect(() => {
     fetch("products.json")
       .then((res) => res.json())
       .then((data) => setProducts(data));
   }, []);
+const handleAddToCart = (product) => {
+  const newCart = [...cart, product];
+  setCart(newCart);
+};
+
   return (
     <div className="shop-container">
       <div className="product-container">
         {products.map((product) => (
           <Product key={product.id}
-            product={product} />
+            product={product}
+          handleAddToCart={handleAddToCart}
+          />
         ))}
       </div>
       <div className="cart-container">
-        <h1>shop container</h1>
+        <h1>Order summary</h1>
+        <p>Selected Items: {cart.length}</p>
       </div>
     </div>
   );
