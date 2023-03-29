d995d1bb2da215f7e5b8ec4ff0785b439dc8d44f
Ashik-Faysal <ashikfaysal.rc@gmail.com>
2023-03-29 11:32:37 +0600

    included localStorage

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -7,9 +7,15 @@ const Cart = ({ cart }) => {
 
   let totalPrice = 0;
   let totalShipping = 0;
-    for (const product of cart) {
-      totalPrice = totalPrice + product.price;
+  let quantity = 0;
+  for (const product of cart) {
+    if (product.quantity === 0) {
+      product.quantity = 1;
+    }
+    // product.quantity = product.quantity || 1;
+      totalPrice = totalPrice + product.price * product.quantity;
       totalShipping = totalShipping + product.shipping;
+      quantity =quantity +product.quantity;
   }
   const tax = totalPrice * 7 / 100;
   const grandTotal = totalPrice + totalShipping + tax; 
@@ -17,7 +23,7 @@ const Cart = ({ cart }) => {
   return (
     <div className="cart">
       <h1>Order summary</h1>
-      <p>Selected Items: {cart.length}</p>
+      <p>Selected Items: {quantity}</p>
       <p>Total Price: {totalPrice}</p>
       <p>Shipping :{totalShipping} </p>
       <p>Tax: {tax.toFixed(2)}</p>
