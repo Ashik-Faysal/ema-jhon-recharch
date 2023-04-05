f1bc394d08184d21ef4ef42393c417443a83a92a
Ashik-Faysal <ashikfaysal.rc@gmail.com>
2023-04-05 10:35:12 +0600

    added order section

--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -1,9 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Cart from '../Cart/Cart';
+import { Link, useLoaderData } from 'react-router-dom';
+import ReviewItem from '../ReviewItem/ReviewItem';
+import './Orders.css';
+import { deleteShoppingCart, removeFromDb } from '../../utilities/fakedb';
 
 const Orders = () => {
+    const savedCart = useLoaderData();
+    const [cart, setCart] = useState(savedCart);
+
+    const handleRemoveFromCart = (id) => {
+        const remaining = cart.filter(product => product.id !== id);
+        setCart(remaining);
+        removeFromDb(id);
+    }
+
+    const handleClearCart = () => {
+        setCart([]);
+        deleteShoppingCart();
+    }
+
     return (
-        <div>
-            <h1>Orders component</h1>
+        <div className='shop-container'>
+            <div className='review-container'>
+                {
+                    cart.map(product => <ReviewItem
+                        key={product.id}
+                        product={product}
+                        handleRemoveFromCart={handleRemoveFromCart}
+                    ></ReviewItem>)
+                }
+            </div>
+            <div className='cart-container'>
+                <Cart
+                    cart={cart}
+                    handleClearCart={handleClearCart}
+                >
+                    <Link className='proceed-link' to="/checkout">
+                        <button className='btn-proceed'>Proceed Checkout</button>
+                    </Link>
+                </Cart>
+            </div>
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,41 +1,51 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Home from "./components/Layout/Home";
-import Shop from "./components/Shop/Shop";
-import Orders from "./components/Orders/Orders";
-import Inventory from "./components/Inventory/Inventory";
-import Login from "./components/Login/Login";
-import Header from "./components/Header/Header";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Shop from './components/Shop/Shop';
+import Home from './components/Layout/Home';
+import Orders from './components/Orders/Orders';
+import Inventory from './components/Inventory/Inventory';
+import Login from './components/Login/Login';
+import cartProductsLoader from './loaders/cartProductsLoader';
+import Checkout from './components/Checkout/Checkout';
+
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <Home />,
+    path: '/',
+    element: <Home></Home>,
     children: [
       {
-        path: "/",
-        element: <Shop />,
+        path: '/',
+        element: <Shop></Shop>
       },
       {
-        path: "orders",
-        element: <Orders />,
+        path: 'orders',
+        element: <Orders></Orders>,
+        loader: cartProductsLoader
       },
       {
-        path: "inventory",
-        element: <Inventory />,
+        path: 'inventory',
+        element: <Inventory></Inventory>
       },
       {
-        path: "login",
-        element: <Login />,
+        path:'checkout',
+        element: <Checkout></Checkout>
       },
-    ],
-  },
-]);
+      {
+        path: 'login',
+        element: <Login></Login>
+      }
+    ]
+  }
+])
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
